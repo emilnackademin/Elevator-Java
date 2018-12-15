33163b627f58c7e4a8ce38de41ea8d8c71da35d5
MrEjonzzon <42805196+MrEjonzzon@users.noreply.github.com>
2018-12-15 21:05:13 +0100

    Update ElevatorHandler.java

--- ElevatorHandler.java
+++ ElevatorHandler.java
@@ -2,31 +2,44 @@ import java.util.Scanner;
 
 public class ElevatorHandler {
 
-    public static void main(String[] args) {
-	boolean repeat = true;
-	Elevator elevator = new Elevator();
-	Scanner scanner = new Scanner(System.in);
-	while (repeat) {
-	    System.out.println("You are currently on floor: " + elevator.getCurrentFloor());
-	    System.out.println("Which floor would you like to go to?");
-	    int selectFloor = scanner.nextInt();
+	public static void main(String[] args) {
+		boolean selection = true;
+		boolean elevatorOn = true;
+		Elevator elevator = new Elevator((int) (Math.random() * 20 + 0), "Emil Elevators Inc #1337");
+		Scanner scanner = new Scanner(System.in);
 
-	    if (selectFloor >= elevator.getBaseFloor() && selectFloor <= elevator.getTopFloor()) {
-		if (selectFloor > elevator.getCurrentFloor()) {
-		    elevator.goUp(selectFloor, elevator.getCurrentFloor());
-		} else if (selectFloor < elevator.getCurrentFloor()) {
-		    elevator.goDown(selectFloor, elevator.getCurrentFloor());
-		} else if (selectFloor == elevator.getCurrentFloor()) {
-		    System.out.println("You are already on that floor");
+		while (selection) {
+			System.out.println("Please choose one of the following:");
+			System.out.println("1 to move elevator");
+			System.out.println("2 to exit elevator");
+			System.out.println("3 to view elevator information");
+
+			int userSelection = scanner.nextInt();
+			switch (userSelection) {
+			case 1:
+				while (elevatorOn) {
+					System.out.println("You are currently on floor: " + elevator.getCurrentFloor());
+					System.out.println("Which floor would you like to go to?");
+					int selectFloor = scanner.nextInt();
+					elevator.MoveElevator(selectFloor);
+					break;
+				}
+				break;
+
+			case 2:
+				System.out.println("Pling... Thank you for using " + elevator.getElevatorName());
+				System.exit(0);
+				break;
+
+			case 3:
+				elevator.elevatorInfo();
+				break;
+
+			default:
+				System.out.println("Invalid selection, try again");
+			}
+
 		}
-	    } else if (selectFloor > elevator.getTopFloor()) {
-		System.out.println("This building's highest floor is: " + elevator.getTopFloor());
-
-	    } else {
-		System.out.println("This building's lowest floor is: " + elevator.getBaseFloor());
-	    }
+		scanner.close();
 	}
-	scanner.close();
-    }
-
 }
